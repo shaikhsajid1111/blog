3feb8d91c21aa4ee8f48a9fa9cb13a50292fa301
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-04-24 12:14:27 +0530

    added user registration form & login

--- users/urls.py
+++ users/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('posts.urls')),
-    path("signup/",include("users.urls")),
+    path('signup/',views.signup_handle,name = "signup"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,Http404,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
+def signup_handle(request):
+    if request.method == "POST":
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password  = request.POST['password_confirm']
+        if password == confirm_password:
+            my_user = User.objects.create_user(username,password)
+            my_user.first_name = fname
+            my_user.last_name = lname
+            my_user.save()
+            return HttpResponse("Saved Successfully!")
+        else:
+            return HttpResponse("Passwords does not match!")
