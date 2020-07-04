6987f98c74a9c1ff5dbdb652b3cc844e1295564c
shaikhsajid1111 <shaikhsajid11112000@gmail.com>
2020-07-04 12:01:04 +0530

    added search functionality

--- posts/views.py
+++ posts/views.py
@@ -10,11 +10,7 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-""" class searchView(ListView):
-   template_name = 'search.html'
-   def get(self,request):
-       query = request.GET['query']
-        """
+
 def search(request):
     query = request.GET['query']
-    render(request,"search.html",{"post_list":Post.objects.all()})        
+    return render(request,"search.html",{"post_list": Post.objects.filter(title__icontains = query)})        
